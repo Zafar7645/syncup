bac07ac004c59faafe2b148ef856334c2fe14e3a
Zafar Shaikh <zafar.shaikh.se@gmail.com>
2025-10-13 19:39:35 +0530

    feat(backend): integrate TypeORM with PostgreSQL support

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
