9a242609ab4a855a2a511a79383c8e438331ede9
Zafar Shaikh <zafar.shaikh.se@gmail.com>
2025-10-18 19:35:01 +0530

    test(auth): add unit tests for register method in auth controller

--- apps/backend/src/auth/auth.controller.spec.ts
+++ apps/backend/src/auth/auth.controller.spec.ts
@@ -1,5 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { UserResponseDto } from 'src/users/user-response.dto';
+
+const mockAuthService = {
+  register: jest.fn(),
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,12 +14,46 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should call the register method of the Auth Service', async () => {
+      // Arrange
+      const mockRegisterUserDto: RegisterUserDto = {
+        name: 'Test User',
+        email: 'test.user@test.com',
+        password: 'StrongPassword@123',
+      };
+      const mockUserResponseDto: UserResponseDto = {
+        id: 1,
+        name: 'Test User',
+        email: 'test.user@test.com',
+      };
+      mockAuthService.register.mockResolvedValue(mockUserResponseDto);
+
+      // Act
+      const actualUserResponseDto =
+        await controller.register(mockRegisterUserDto);
+
+      // Assert
+      expect(mockAuthService.register).toHaveBeenCalledWith(
+        mockRegisterUserDto,
+      );
+      expect(actualUserResponseDto).toEqual(mockUserResponseDto);
+    });
   });
 });
